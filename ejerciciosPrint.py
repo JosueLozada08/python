8f734c405d0ff3a084b7b4a1366b2ca00a5c9a9a
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-15 20:10:37 -0500

    primeros commit de ejercicios print

---
+++ ejerciciosPrint.py
@@ -0,0 +1,14 @@
+print("hola mundo ") 
+
+
+ #primer ejercicio
+ 
+print("Me encanta estudiar python")
+
+#segundo ejercicio
+
+print('Estudiar con "Python Total" es super divertido')
+
+#tercer ejercicio
+
+print(555)
