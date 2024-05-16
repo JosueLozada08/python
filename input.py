37ff47828112b0088e8a1f83a1f858e1f2bd9215
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-15 21:23:44 -0500

    input commit

---
+++ input.py
@@ -0,0 +1,18 @@
+print(input("Ingresa tu nombre: "))
+print(input("Ingrsa tu apellido: "))
+print("Tu nombre es: "+input("Ingresa tu nombre: ") + " " + input("Ingrsa tu apellido: "))
+
+#EJERCICIOS INPUT 
+
+#ejercicio 1
+respuesta = input("¿Qué estás estudiando? ")
+print(respuesta)
+
+#ejercicio 2
+respuesta = input("¿En qué país vives?")
+print(respuesta)
+
+#ejercicio 3
+nombre= input("Escribe tu nombre: ")
+apellido= input("Escribe tu apellido: ")
+print(nombre + apellido )
