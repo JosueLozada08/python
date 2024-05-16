bdc30c60a15611d60f3d967a61367caa1b6cb4a8
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-15 20:46:08 -0500

    ejercicios string

---
+++ string.py
@@ -0,0 +1,35 @@
+print("Hola"+ " "+ 'josue')
+
+
+#llave invertida 
+
+print("Hola me llamo \"josue\" ")
+
+#escribe dos lineas seguidas 
+
+print("Esta es una linea \ny esta es otra linea") #salto de linea 
+
+print("\tEsta es la tercera liena ") #la \t es para tabular 
+
+print('This isn\'t a number')#\' es para que lea la comilla como lo que es 
+
+print('Este signo \\ es una barra imbertida ')#\\ es para que se pueda imprimir la barra imbertida 
+
+
+#EJERCICIOS DEL CURSO 
+
+
+#1
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+#2 PIDE IMPRESION POR TABULACION 
+
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+
+#3
+
+print("Barra Normal: /")
+print("Barra Invertida: \\")
