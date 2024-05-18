279c7e554d37192987a093243a6b84eefafedfb9
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-17 22:39:28 -0500

    variables py

---
+++ variables.py
@@ -0,0 +1,21 @@
+nombre = "Josue"
+print(nombre)
+
+nombre = "Juan"
+print(nombre)
+
+#Ejercicios
+#Ejercicio 1
+nombre = "Tony Soprano"
+edad = 51
+
+#Ejercicio 2
+nombre = "Julia"
+apellido = "Roberts"
+nombrecompleto = nombre + " " + apellido
+
+#Ejercicio 3
+curso = "Python"
+
+print("Estás tomando un curso de" + " " + curso)
+
