0fd0463ddcb8acce5cb4c5f83be4420cf6b2acb8
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-21 21:19:19 -0500

    format

---
+++ Dia 2/FormatearCadenas.py
@@ -0,0 +1,32 @@
+x = 10
+y =  5
+
+print("Mis numeros son :" + str(x) + " y " + str(y))
+
+print("Mis numeros son :{} y {}".format(x,y))
+
+print("La suma de {} y {} es igual a {}".format(x,y,x+y))
+
+
+#ejemplo
+color =  "rojo"
+matricula = 556644
+
+print(f"El auto es {color} y su matricula es {matricula}")#el f (format) es para hacerlo una cadena literal 
+
+#EJERCICIOS
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print("Estimado/a {}, su número de asociado es: {}".format(nombre_asociado, numero_asociado))
+
+#2
+puntos_nuevos = 350
+puntos_totales = 1225
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+#3
+puntos_anteriores = 875
+puntos_nuevos = 350
+print(f"Has ganado {puntos_nuevos} puntos! En total acumulas {puntos_anteriores+puntos_nuevos} puntos")
--- Dia 2/convercion.py
+++ Dia 2/convercion.py
@@ -43,5 +43,3 @@ num1 = "7.5"
 num2 = "10"
 
 print(float(num1) + float(num2))
-
-
