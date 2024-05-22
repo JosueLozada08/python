4cba70bb4ded21982c8eb4c0062316bc8d820f4e
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-21 20:20:43 -0500

    integer y float

---
+++ Dia 2/integer_float.py
@@ -0,0 +1,40 @@
+mi_numero = 1
+print(mi_numero)
+print(type(mi_numero))
+
+#ejemplo
+edad= input("Dime tu edad")
+print("tu edad es:" + edad)
+
+nuevaEdad = 1+edad
+print("Vas a cumplir " + nuevaEdad)
+
+#HAY UN ERROR YA QUE COMO EDAD ES UN STRING NO SE PUEDE SUMAR, HAYQ UE CONVERTIRLO A INTEGER
+
+#Declara una variable numérica llamada num_entero que contenga un valor de tipo integer de tu elección.
+#Imprime el tipo de dato de dicha variable.
+
+num_entero = 4
+
+print(type(num_entero))
+
+
+#Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección.
+#Imprime el tipo de dato de dicha variable
+
+num_decimal = 5.7
+print(type(num_decimal))
+
+#¿De qué tipo es el resultado de la suma de 7.5 + 2.5? Genera el código para verificarlo.
+#Para ello, crea dos variables:
+#num1 = 7.5
+#num2 = 2.5
+#A continuación, muestra en pantalla el tipo de dato que resulta de la suma de ambos números.
+#Realiza el mismo ejercicio en PyCharm para ver el resultado. ¿Coindice con lo que esperabas?
+
+num1 = 7.5
+num2 = 2.5
+
+suma = num2+num1
+print(type(suma))
+
