ce91a6dc1b7c221f84bbcca50e53e22c5dc8f3af
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-22 21:46:26 -0500

    commit de redondeo

---
+++ Dia 2/redondeo.py
@@ -0,0 +1,37 @@
+print("primer ejercicio")
+resultado = round(90/7)
+
+print(resultado)
+print(type(resultado))
+
+
+valor = 95.666666666666666666#round pide dos valores, el numero como tal y el segundo la cantidad de decimales que quieres 
+print(round(valor,3))
+print(type(valor))
+
+
+#ejercicios 
+#1
+resultado = 10/3
+print(round(resultado,2))
+
+#2
+valor = 10.676767
+print(round(valor))
+
+
+#3 sacar raiz cuadrada de 5 y redondear con 4 decimales 
+num=5**0.5
+print("raiz cuadrada:")
+print(num)
+print(round(num,4))
+
+print("codigo de prueba")
+num1 = 10
+num2 = 25
+num1=num2
+print(num1)
+print(num2)
+
+
+print(type(7.5 + 2.5))
