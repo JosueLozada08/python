67db386ff6145659ea24cbd9bd005477d78f027e
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-22 22:18:13 -0500

    index

---
+++ Dia 3/index.py
@@ -0,0 +1,18 @@
+miTexto = "Esta es una prueba"
+resultado = miTexto.index("u",9)
+print(resultado)
+
+
+print("Ejercicio 1")
+palabra = "ordenador"
+resultado = palabra[4]
+print(resultado)
+
+print("Ejercicio 2")
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+resultado = frase.index("práctica")
+print(resultado)
+
+print("Ejercicio 3")
+resultado = frase.index("práctica",28)
+print(resultado)
