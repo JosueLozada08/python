82bb41fcfba50f94feeed3cf7c270885d9d4ebbd
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-05-23 22:23:06 -0500

    propiedades string

---
+++ Propiedades.py
@@ -0,0 +1,41 @@
+nombre = "Carina"
+nombre = "k"
+print("karina")
+
+
+n1 = "Kari"
+n2 = "na"
+print(n1+n2)
+
+
+poema = """mil pequeños peces blancos 
+comsi hirviera
+el color del agua """
+print(poema)
+print("agua" in poema)
+print("agua" not in poema)
+
+
+print(len(poema)) #imprime la cantidad de caracteres incluyendo espacios 
+
+
+print("Ejercicios 1")
+# Definimos el texto a repetir
+texto = "Repetición"
+
+# Concatenamos el texto 15 veces
+resultado = texto * 15
+
+# Mostramos el resultado en pantalla
+print(resultado)
+
+print("Ejercicios 2")
+
+
+
+
+print("Ejercicio 3 ")
+
+
+
+
