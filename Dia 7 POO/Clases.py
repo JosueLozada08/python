33a46cd66e1bdf19407f8be80ab47ded4faf09b3
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-06-05 21:26:47 -0500

    clases dia7

---
+++ Dia 7 POO/Clases.py
@@ -0,0 +1,32 @@
+class pajaro:
+    pass #para que no nos de error ya que aun no sabemos que hacer dentro de la clase 
+
+
+miPajaro = pajaro()
+
+otroPajaro=pajaro()
+
+print(miPajaro)
+print(otroPajaro)
+
+print(type(miPajaro))
+
+print("Ejercicio 1")
+class Personaje:
+    pass
+
+harry_potter = Personaje()
+print("Ejercicio 2")
+class Dinosaurio:
+    pass
+
+velociraptor = Dinosaurio()
+tiranosaurio_rex = Dinosaurio()
+braquiosaurio  = Dinosaurio()
+print("Ejercicio 3")
+class PlataformaStreaming:
+    pass
+
+netflix = PlataformaStreaming()
+hbo_max  = PlataformaStreaming()
+amazon_prime_video  = PlataformaStreaming()
