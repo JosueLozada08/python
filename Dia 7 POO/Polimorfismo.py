d34d08f3ebd2c832852829e52ba52f3970427eb9
JosueLozada08 <129800585+JosueLozada08@users.noreply.github.com>
2024-06-29 20:58:44 -0500

    polimorfismo completo

--- Dia 7 POO/Polimorfismo.py
+++ Dia 7 POO/Polimorfismo.py
@@ -33,6 +33,67 @@ animal_hablar(oveja1)
 animal_hablar(vaca1)
   
 print("Ejercicio 1")
+# Definición de los objetos
+palabra = "polimorfismo"
+lista = ["Clases", "POO", "Polimorfismo"]
+tupla = (1, 2, 3, 80)
+
+# Creación de una lista con los objetos
+objetos = [palabra, lista, tupla]
+
+# Iterar sobre cada objeto y mostrar su longitud
+for obj in objetos:
+    print(len(obj))
+
 print("Ejercicio 2")
+class Mago():
+    def atacar(self):
+        print("Ataque mágico")
+
+class Arquero():
+    def atacar(self):
+        print("Lanzamiento de flecha")
+
+class Samurai():
+    def atacar(self):
+        print("Ataque con katana")
+
+# Crear instancias de cada clase
+mago = Mago()
+arquero = Arquero()
+samurai = Samurai()
+
+# Crear una lista con los personajes en el orden especificado
+personajes = [arquero, mago, samurai]
+
+# Iterar sobre la lista y llamar al método atacar() de cada personaje
+for personaje in personajes:
+    personaje.atacar()
+
 print("Ejercicio 3")
-        
+""" class Mago():
+    def defender(self):
+        print("Escudo mágico")
+
+class Arquero():
+    def defender(self):
+        print("Esconderse")
+
+class Samurai():
+    def defender(self):
+        print("Bloqueo")
+
+def personaje_defender(personaje):
+    personaje.defender()
+
+# Crear instancias de cada clase
+mago = Mago()
+arquero = Arquero()
+samurai = Samurai()
+
+# Probar la función con diferentes personajes
+personaje_defender(mago)       # Salida: Escudo mágico
+personaje_defender(arquero)    # Salida: Esconderse
+personaje_defender(samurai)    # Salida: Bloqueo
+
+         """
