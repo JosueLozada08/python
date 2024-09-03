82b201023e5a793216ca9a352e1e7096543976f3
Josue lozada <129800585+JosueLozada08@users.noreply.github.com>
2024-09-02 21:15:26 -0500

    ultimos cambios

--- ejercicioRandomValidacion.py
+++ ejercicioRandomValidacion.py
@@ -3,9 +3,9 @@ import random
 print("Juego de números random")
 
 while True:
+    
     numRandom = input("Ingresa un número del 1 al 10: ")
-    
-    
+       
     if numRandom.isdigit():  #valida si esta compuesto por digitos 
         numRandom = int(numRandom)
         if 1 <= numRandom <= 10:  #revisar si esta en el rango 
@@ -16,6 +16,7 @@ while True:
         print("Entrada inválida. Por favor, ingresa un número válido.")
 
 numProgram = random.randint(1, 10)
+#si el valor fuera flotante random.uniform(a, b)
 
 if numRandom == numProgram:
     print(f"Adivinaste, el número correcto era: {numProgram}")
--- ejerciciox/ejerciciorandom.py
+++ ejerciciox/ejerciciorandom.py
@@ -1,11 +1,16 @@
 import random
 
+
 print("juego de numeros random")
-numRandom= int(input("ingresa un numero del 1 al 10: "))
+  
+numRandom =  int(input("ingresa un numero del 1 al 10: "))
 
 numProgram = random.randint(1,10)
 
 if numRandom == numProgram:
+    
     print(f"adivinaste, el numero correcto era: {numProgram}")
-else:
-    print(f"perdiste, el nuemro correcto era: {numProgram}")
+    
+else:    
+
+    print(f"perdiste, el nuemro correcto era: {numProgram}")   
